7a3c0d905ba67068b9dc1b5963541fd07d9fdcaa
Martin Graversen <martin@graversen.io>
2019-01-10 20:01:10 +0100

    HashingTransformers to plug in hashing transformers

---
+++ src/main/java/io/graversen/arrow/HashingTransformers.java
@@ -0,0 +1,34 @@
+package io.graversen.arrow;
+
+import io.graversen.trunk.hashing.DigestAlgorithms;
+import io.graversen.trunk.hashing.DigestUtils;
+
+public class HashingTransformers
+{
+    private static final DigestUtils DIGEST_UTILS = new DigestUtils();
+
+    private HashingTransformers()
+    {
+
+    }
+
+    public static Transformer md5()
+    {
+        return generic(DigestAlgorithms.MD5);
+    }
+
+    public static Transformer sha256()
+    {
+        return generic(DigestAlgorithms.SHA256);
+    }
+
+    public static Transformer sha512()
+    {
+        return generic(DigestAlgorithms.SHA512);
+    }
+
+    private static Transformer generic(String digestAlgorithm)
+    {
+        return input -> DIGEST_UTILS.computeHashHex(input, digestAlgorithm);
+    }
+}
