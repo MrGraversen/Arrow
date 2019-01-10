8700f341acb93f65f3da528647b9db716d4f1e3c
Martin Graversen <martin@graversen.io>
2019-01-10 22:54:27 +0100

    Remove redundant counting

--- src/main/java/io/graversen/arrow/ArrowJob.java
+++ src/main/java/io/graversen/arrow/ArrowJob.java
@@ -1,10 +1,7 @@
 package io.graversen.arrow;
 
-import io.graversen.trunk.math.MathUtils;
-
 import java.util.Collection;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -16,7 +13,6 @@ class ArrowJob implements Callable<ArrowResult>
     private final Collection<Transformer> transformers;
     private final Collection<Logger> loggers;
 
-    private final AtomicLong counter;
     private final Value<String> result;
 
     public ArrowJob(String target, CombinationsService combinationsService, Collection<Transformer> transformers, Collection<Logger> loggers)
@@ -26,7 +22,6 @@ class ArrowJob implements Callable<ArrowResult>
         this.transformers = transformers;
         this.loggers = loggers;
 
-        this.counter = new AtomicLong(0);
         this.result = Value.empty();
     }
 
@@ -37,23 +32,17 @@ class ArrowJob implements Callable<ArrowResult>
 
         combinationsService.computeCombinations()
                 .peek(remember())
-                .peek(count())
                 .map(applyTransformers())
                 .peek(log())
                 .filter(checkCombination())
                 .findFirst();
 
-        return new ArrowResult(result.value(), counter.get(), start, System.currentTimeMillis());
+        return new ArrowResult(result.value(), combinationsService.getCombinationsGenerated(), start, System.currentTimeMillis());
     }
 
     private Consumer<String> remember()
     {
         return result::update;
-    }
-
-    private Consumer<String> count()
-    {
-        return combination -> counter.incrementAndGet();
     }
 
     private Consumer<String> log()
