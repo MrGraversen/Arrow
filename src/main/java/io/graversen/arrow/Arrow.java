8dda2085981c320e1316d6c70a1f087d27d65639
Martin Graversen <martin@graversen.io>
2019-01-10 19:34:37 +0100

    Arrow async combination traversal

--- src/main/java/io/graversen/arrow/Arrow.java
+++ src/main/java/io/graversen/arrow/Arrow.java
@@ -1,9 +1,34 @@
 package io.graversen.arrow;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Arrow
 {
-    public static void main(String[] args)
-    {
+    private final CombinationsService combinationsService;
+    private final ExecutorService combinationsExecutorService;
 
+    public Arrow(CombinationsService combinationsService)
+    {
+        this.combinationsService = combinationsService;
+        this.combinationsExecutorService = Executors.newSingleThreadExecutor();
+    }
+
+    public Future<String> tryFind(String target)
+    {
+        return tryFind(target, Collections.emptyList());
+    }
+
+    public Future<String> tryFind(String target, Transformer transformer)
+    {
+        return tryFind(target, Collections.singletonList(transformer));
+    }
+
+    public Future<String> tryFind(String target, Collection<Transformer> transformers)
+    {
+        return combinationsExecutorService.submit(new ArrowJob(target, combinationsService, transformers));
     }
 }
