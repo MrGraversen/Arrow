67d9549f61d2d8f42c22edd01e65c8be852d3618
Martin Graversen <martin@graversen.io>
2019-01-10 20:47:55 +0100

    Draft of generate, transform, check chain

--- src/main/java/io/graversen/arrow/ArrowJob.java
+++ src/main/java/io/graversen/arrow/ArrowJob.java
@@ -1,37 +1,59 @@
 package io.graversen.arrow;
 
-import io.graversen.trunk.hardware.HardwareUtils;
-
 import java.util.Collection;
-import java.util.Queue;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 class ArrowJob implements Callable<String>
 {
     private final String target;
     private final CombinationsService combinationsService;
     private final Collection<Transformer> transformers;
+    private final Collection<Logger> loggers;
 
-    private final Queue<String> combinationQueue;
-    private final ExecutorService transformersExecutorService;
+    private final Value<String> result;
 
-    public ArrowJob(String target, CombinationsService combinationsService, Collection<Transformer> transformers)
+    public ArrowJob(String target, CombinationsService combinationsService, Collection<Transformer> transformers, Collection<Logger> loggers)
     {
         this.target = target;
         this.combinationsService = combinationsService;
         this.transformers = transformers;
-
-        final int processorCount = HardwareUtils.getHardware().getLogicalProcessorCount();
-        this.combinationQueue = new ConcurrentLinkedQueue<>();
-        this.transformersExecutorService = Executors.newFixedThreadPool(processorCount);
+        this.loggers = loggers;
+        this.result = Value.empty();
     }
 
     @Override
-    public String call() throws Exception
+    public String call()
     {
-        return "";
+        combinationsService.computeCombinations()
+                .peek(remember())
+                .map(applyTransformers())
+                .peek(log())
+                .filter(checkCombination())
+                .findFirst();
+
+        return result.value();
+    }
+
+    private Consumer<String> remember()
+    {
+        return result::update;
+    }
+
+    private Consumer<String> log()
+    {
+        return StreamUtils.combine(loggers.toArray(Logger[]::new));
+    }
+
+    private Function<String, String> applyTransformers()
+    {
+        return StreamUtils.combine(transformers.toArray(Transformer[]::new));
+    }
+
+    private Predicate<String> checkCombination()
+    {
+        return target::equals;
     }
 }
